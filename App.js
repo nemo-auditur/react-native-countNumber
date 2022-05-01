1a2a11295ecef9fde025b4126e717b06aa3a2c73
Clément Legal <clement@macbook-pro-de-clement.home>
2022-05-01 22:15:41 +0200

    add comments

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
+//imports 
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import React, {useState} from 'react';
 
 export default function App() {
 
+  // Gestion des states
   const [sentence, setSentence] = useState('')
   const [isDisabled, setIsDisabled] = useState(true)
 
+  // fontion prenant en entrée une string
+  // compte le nom de characters par mot de la string
   const displayNumbers = (sentence) => {
     return sentence
       .split(' ')
@@ -14,6 +18,7 @@ export default function App() {
       .join(' ')
   }
 
+  // function permettant de nettoyer les entrée de l'input
   const onClean = () => {
     setSentence('')
     setIsDisabled(true)
